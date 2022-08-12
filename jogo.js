d54b846d6008ae18f8b7e4372d636f95b383dbde
rlsena <rlsena@gmail.com>
2022-08-12 15:21:30 -0300

    bootstrap inserido

--- jogo.js
+++ jogo.js
@@ -4,47 +4,47 @@ function game(choice){
 
     if((choice == 1)&&(oponente==1)){
 
-        console.log("empatou");
+        document.getElementById("result").innerHTML = "empatou";
 
     }
     else if((choice == 1)&&(oponente==2)){
 
-        console.log("Papel! Você perdeu");
+        document.getElementById("result").innerHTML = "Papel! Você perdeu";
 
     }
     else if((choice == 1)&&(oponente==3)){
 
-        console.log("Tesoura! Você ganhou");
+        document.getElementById("result").innerHTML = "Tesoura! Você ganhou";
 
     }
     else if((choice == 2)&&(oponente==1)){
 
-        console.log("Pedra! Você ganhou");
+        document.getElementById("result").innerHTML = "Pedra! Você ganhou";
 
     }
     else if((choice == 2)&&(oponente==2)){
 
-        console.log("empatou");
+        document.getElementById("result").innerHTML = "empatou";
 
     }
     else if((choice == 2)&&(oponente==3)){
 
-        console.log("Tesoura! Você perdeu");
+        document.getElementById("result").innerHTML = "Tesoura! Você perdeu";
 
     }
     else if((choice == 3)&&(oponente==1)){
 
-        console.log("Pedra! Você perdeu");
+        document.getElementById("result").innerHTML = "Pedra! Você perdeu";
 
     }
     else if((choice == 3)&&(oponente==2)){
 
-        console.log("Papel! Você ganhou");
+        document.getElementById("result").innerHTML = "Papel! Você ganhou";
 
     }
     else if((choice == 3)&&(oponente==3)){
         
-        console.log("empatou");
+        document.getElementById("result").innerHTML = "empatou";
     } 
 
     
